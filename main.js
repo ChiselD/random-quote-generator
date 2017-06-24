cbe75be7ef8056c03111846a36faaba061a8e909
ChiselD <chisel.delahanty@gmail.com>
2017-06-23 21:58:43 -0400

    create quote library (array)

---
+++ main.js
@@ -0,0 +1,50 @@
+let quotes = [
+  {
+  	quote: "Don't feel entitled to anything you didn't sweat and struggle for.",
+  	source: "Marian Wright Edelman"
+  },
+  {
+  	quote: "The greatest gift is not being afraid to question.",
+  	source: "Ruby Dee"
+  },
+   {
+  	quote: "The most difficult thing is the decision to act, the rest is merely tenacity.",
+  	source: "Amelia Earhart"
+  },
+  {
+  	quote: "Done is better than perfect.",
+  	source: "Sheryl Sandberg"
+  },
+  {
+  	quote: "No matter what accomplishments you make, somebody helped you.",
+  	source: "Althea Gibson"
+  },
+  {
+  	quote: "A surplus of effort could overcome a deficit of confidence.",
+  	source: "Sonia Sotomayor"
+  },
+  {
+  	quote: "There are two kinds of people, those who do the work and those who take the credit. Try to be in the first group; there is less competition there.",
+  	source: "Indira Gandhi"
+  },
+  {
+  	quote: "Optimism is the faith that leads to achievement.",
+  	source: "Helen Keller"
+  },
+  {
+  	quote: "In the work of every genius we recognize our own rejected thoughts.",
+  	source: "Johann Wolfgang von Goethe"
+  },
+  {
+  	quote: "Mistakes are a fact of life. It is the response to the error that counts.",
+  	source: "Nikki Giovanni"
+  },
+  {
+  	quote: "Art is too long and life is too short",
+  	source: "Grace Paley"
+  },
+  {
+  	quote: "Nenne dich nicht arm, weil deine Träume nicht in Erfüllung gegangen sind; wirklich arm ist nur, der nie geträumt hat.",
+  	source: "Marie von Ebner-Eschenbach"
+  },
+];
